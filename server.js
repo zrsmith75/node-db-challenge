9727819686e97073ab28e5d51b2ab2d2adebb241
Zac Smith <zrs3141592@gmail.com>
2019-09-04 13:55:24 -0600

    rebuilding, step-bys-step

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const ProjectRouter = require("./project/project-router.js");
+
+const server = express();
+
+server.use(express.json());
+server.use("/api/projects", ProjectRouter);
+
+module.exports = server;
