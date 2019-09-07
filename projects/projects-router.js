8272c474ae1fdf9353ff1ee3de02e5b5e7b92742
Zac Smith <zrs3141592@gmail.com>
2019-09-07 10:17:28 -0600

    correct package.json

--- projects/projects-router.js
+++ projects/projects-router.js
@@ -2,6 +2,16 @@ const express = require("express");
 const Projects = require("./projects-mdoel");
 const router = express.Router();
 
-router.get("/", (req, res) => {});
+router.get("/", (req, res) => {
+  db("projects")
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: error
+      });
+    });
+});
 
 module.exports = router;
